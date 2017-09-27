55e4cd196c381364921ad2bd02f5cd96fc30722a
elitvinenko <ilitvinenko90@gmail.com>
2017-09-27 14:22:11 +0300

    viewClass + replaceMe -

---
+++ viewClass.h
@@ -0,0 +1,12 @@
+//
+//  viewClass.h
+//  AKGitViewControllers
+//
+//  Created by Developer on 27.09.17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface viewClass : UIView
+
+@end
---
+++ viewClass.m
@@ -0,0 +1,20 @@
+//
+//  viewClass.m
+//  AKGitViewControllers
+//
+//  Created by Developer on 27.09.17.
+//
+
+#import "viewClass.h"
+
+@implementation viewClass
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
